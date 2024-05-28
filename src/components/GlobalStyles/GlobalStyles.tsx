8c1556d928c5f039414a5ccff29aad83fbbc423a
Dave Judge <143704883+lvl7wizard@users.noreply.github.com>
2024-05-28 23:43:21 +0100

    removes app.css and replaces with GlobalStyles from styled-components

---
+++ src/components/GlobalStyles/GlobalStyles.tsx
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+background: #3e4a61;
+  margin: 0;
+  }
+`;
+
+export default GlobalStyle;
