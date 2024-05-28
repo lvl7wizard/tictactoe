2db9a197705fb935b815922a356bd50d775a2c8e
Dave Judge <143704883+lvl7wizard@users.noreply.github.com>
2024-05-28 23:47:09 +0100

    adds imports for new components and refactors moveLog logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import "./App.css";
 import { useState } from "react";
+import Header from "./components/Header/Header";
+import SetUp from "./components/SetUp/SetUp";
 import Grid from "./components/Grid/Grid";
-import Scores from "./components/Scores/Scores";
-import NewGame from "./components/NewGame/NewGame";
+import Results from "./components/Results/Results";
 import Footer from "./components/Footer/Footer";
 import styled from "styled-components";
+import GlobalStyle from "./components/GlobalStyles/GlobalStyles";
 
-const TicTacToeContainer = styled.div`
+const GameContainer = styled.div`
   display: flex;
   flex-direction: column;
   min-height: 100vh;
@@ -15,35 +16,36 @@ const TicTacToeContainer = styled.div`
 
 function App() {
   const [playerOne, setPlayerOne] = useState<boolean>(true);
-  const [userFeedback, setUserFeedback] = useState<string>("");
-  const [moveLog, setMoveLog] = useState<("X" | "O" | "")[]>([
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-  ]);
-  const [gameOver, setGameOver] = useState<boolean>(false);
   const [score, setScore] = useState<{ playerOne: number; playerTwo: number }>({
     playerOne: 0,
     playerTwo: 0,
   });
+  const [usernames, setUsernames] = useState<{ playerOne: string; playerTwo: string; confirmed: boolean }>({
+    playerOne: "",
+    playerTwo: "",
+    confirmed: false
+  });
+  const [userFeedback, setUserFeedback] = useState<string>("");
+  const [moveLog, setMoveLog] = useState<("X" | "O" | "")[]>(Array(9).fill(""));
+  const [gameOver, setGameOver] = useState<boolean>(false);
 
   const resetGame = () => {
-    setMoveLog(["", "", "", "", "", "", "", "", ""]);
+    setMoveLog(Array(9).fill(""));
     setUserFeedback("");
     setGameOver(false);
   };
 
   return (
-    <TicTacToeContainer>
-      <Scores score={score} playerOne={playerOne} />
-      {!gameOver ? (
+    <>
+    <GlobalStyle/>
+    <GameContainer>
+      <Header usernames={usernames} score={score} playerOne={playerOne} />
+      {usernames.confirmed === false && (
+        <SetUp usernames={usernames} setUsernames={setUsernames} />
+      )}
+      {usernames.confirmed === true && !gameOver && (
         <Grid
+        usernames={usernames}
           playerOne={playerOne}
           setPlayerOne={setPlayerOne}
           setUserFeedback={setUserFeedback}
@@ -53,11 +55,13 @@ function App() {
           setGameOver={setGameOver}
           setScore={setScore}
         />
-      ) : (
-        <NewGame userFeedback={userFeedback} resetGame={resetGame} />
+      )}
+      {gameOver && (
+        <Results usernames={usernames} userFeedback={userFeedback} resetGame={resetGame} />
       )}
       <Footer />
-    </TicTacToeContainer>
+    </GameContainer>
+    </>
   );
 }
 
