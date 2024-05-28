f79044f9864f452278ba20db524fa7f0b8321173
Dave Judge <143704883+lvl7wizard@users.noreply.github.com>
2024-05-28 19:31:09 +0100

    deletes UserFeedback component

--- src/components/UserFeedback.tsx
+++
@@ -1,9 +0,0 @@
-interface UserFeedbackProps {
-    userFeedback: string;
-}
-
-function UserFeedback ({userFeedback}: UserFeedbackProps) {
-    return <p className="user-feedback">{userFeedback}</p>
-}
-
-export default UserFeedback;
