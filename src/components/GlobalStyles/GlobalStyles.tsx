cb8adcdbdabd6296898539d51e850da6051f98a6
Dave Judge <143704883+lvl7wizard@users.noreply.github.com>
2024-05-29 00:40:32 +0100

    moves color and font family to GlobalStyles

--- src/components/GlobalStyles/GlobalStyles.tsx
+++ src/components/GlobalStyles/GlobalStyles.tsx
@@ -4,6 +4,8 @@ const GlobalStyle = createGlobalStyle`
   body {
 background: #3e4a61;
   margin: 0;
+  font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+  color: #d9dad7;
   }
 `;
 
